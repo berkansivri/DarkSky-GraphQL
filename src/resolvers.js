672fcc6388e335567debcabb21b593d6bfc8f456
Berkan <berkansivri@gmail.com>
2019-05-12 02:23:28 +0300

    units bug fix

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,11 +1,11 @@
 
 const resolvers = {
   Query: {
-    forecast: async (_, { latitude, longitude }, { dataSources }) => {
-      return dataSources.forecastAPI.getForecast({latitude, longitude, units:"ca"});
+    forecast: async (_, { latitude, longitude, units }, { dataSources }) => {
+      return dataSources.forecastAPI.getForecast({latitude, longitude, units: units || "ca"});
     },
-    forecastByDate: async (_, { latitude, longitude, date }, { dataSources }) => {
-      return dataSources.forecastAPI.getForecastByDate({latitude, longitude, date, units:"ca"});
+    forecastByDate: async (_, { latitude, longitude, date, units }, { dataSources }) => {
+      return dataSources.forecastAPI.getForecastByDate({latitude, longitude, date, units: units || "ca"});
     },
     hello: () => {return "Welcome to DarkSky GraphQL API"}
   }
